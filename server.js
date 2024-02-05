fdfe71d6130aaaed801b2600e559c09c82fb0be2
KelseyKor <kornegay120@gmail.com>
2024-02-04 19:18:35 -0500

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Budget API
 
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+
+app.use('/', express.static('public')); /*this opens the personal budget website onto local host, from public folder*/
 
 app.use(cors());
 
@@ -24,11 +27,27 @@ const budget = {
     ]
 };
 
+app.get('/hello', (req, res) => {   /*would have to go to localhost:3000/hello to see this */
+    res.send('Hello World!');
+});
 
-app.get('/budget', (req, res) => {
+
+app.get('/budget', (req, res) => {  /*creates a new route, so if they go to /budget, they get a response of json, not html, and passes object (const budget above) to be transformed into json*/
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);  /*the address of where my page is rendered*/
 });
+
+
+/*All below follows video @ 5:50 */
+/*
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+*/
